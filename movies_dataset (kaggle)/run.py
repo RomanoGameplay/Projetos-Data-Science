da83f325730d89e29673fec4807af5a31f8d580f
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2023-07-27 16:33:01 -0300

    Adicao do arquivo logs.py

--- movies_dataset (kaggle)/run.py
+++ movies_dataset (kaggle)/run.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import click
 
 from src.aquisicao.executa import executa_etl
-from src.aquisicao.opcoes import EnumMovies
+from src.aquisicao.opcoes import EnumMovies, ETL_DICT
 from src.configs import CAMINHO_ENTRADA, CAMINHO_SAIDA
+from src.utils.logs import configura_logs
 
 
 @click.group()
@@ -35,14 +36,19 @@ def aquisicao() -> None:
 @click.option('--nao-reprocessar', is_flag=True, show_default=True,
               help='Flag indicando necessidade de reprocessar os dados')
 def processa_dado(etl: str, entrada: Path, saida: Path, nao_criar_caminho: bool, nao_reprocessar: bool) -> None:
-    executa_etl(
-        etl=etl,
-        entrada=entrada,
-        saida=saida,
-        criar_caminho=not nao_criar_caminho,
-        reprocessar=not nao_reprocessar
-    )
+    """
+    Comando responsável por executar o pipeline dos dados
+    :param etl: Nome do ETL a ser executado
+    :param entrada: Caminho de entrada dos dados
+    :param saida: Caminho de saída para os dados
+    :param nao_criar_caminho: Flag indicando necessidade de criar caminho para os dados
+    :param nao_reprocessar: Flag indicando necessidade de reprocessamento dos dados
+    """
+    configura_logs()
+    obj = ETL_DICT[EnumMovies(etl)](entrada, saida, nao_criar_caminho, nao_reprocessar)
+    obj.pipeline()
 
 
 if __name__ == '__main__':
     grupo_principal()
+    # Executar o tratamento dos dados e configuração de arquivos ymal para renomear colunas.
