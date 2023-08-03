55d619483b06e9611cc943599bad18c347dfc31e
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2023-08-02 21:28:03 -0300

    correcoes

--- movies_dataset (kaggle)/src/tests/utils/info.py
+++ movies_dataset (kaggle)/src/tests/utils/info.py
@@ -1,7 +1,7 @@
 from src.utils.info import carrega_yaml
 
 
-def test_carrega_yaml():
+def test_carrega_yaml() -> None:
     info = carrega_yaml('aquisicao_movies.yml')
     assert isinstance(info, dict)
     assert 'RENOMEIA_COLUNAS' in info
