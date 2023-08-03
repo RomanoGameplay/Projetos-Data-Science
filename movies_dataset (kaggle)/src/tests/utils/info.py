c36d9da38b917f2b91f011b996cba261f49606c6
RomanoGameplay <82478812+RomanoGameplay@users.noreply.github.com>
2023-08-02 21:25:42 -0300

    termino de criacao dos testes no test_info.py

---
+++ movies_dataset (kaggle)/src/tests/utils/info.py
@@ -0,0 +1,8 @@
+from src.utils.info import carrega_yaml
+
+
+def test_carrega_yaml():
+    info = carrega_yaml('aquisicao_movies.yml')
+    assert isinstance(info, dict)
+    assert 'RENOMEIA_COLUNAS' in info
+    assert 'DADOS_SCHEMA' in info
